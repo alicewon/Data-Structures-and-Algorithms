afcaf40ef36d3032381e6177d5473d11930dca3b
Alice Won <alicewon4@gmail.com>
2020-06-05 17:36:40 -0700

    updated variation on battleship

--- leetcode/419BattleshipCount.js
+++ leetcode/419BattleshipCount.js
@@ -18,7 +18,7 @@ var countBattleships = function(board) {
 }
 
   function sink(board, i, j) {
-      //check if neighboring cell is an X, we
+      //check: if neighboring cell is an X
       if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != "X") {
           return 
       } else
@@ -28,3 +28,24 @@ var countBattleships = function(board) {
       sink(board, i, j +1)
       
   }
+
+// josh's answer:
+//   var countBattleships = function(board) {
+//     let numOfShips = 0
+//     for(let i = 0; i< board.length; i++){
+//         for(let j = 0; j < board[i].length; j++){
+//             if (board[i][j] === 'X'){
+//                 if (!hasBeenCounted(board, i, j)){
+//                     numOfShips++
+//                 }
+//             }
+//         }
+//     }
+//     return numOfShips
+// };
+
+// const hasBeenCounted = (board, i, j) => {
+//     if (i > 0 && board[i-1][j] === 'X') return true
+//     if (j > 0 && board[i][j-1] === 'X') return true 
+//     return false 
+// } 
