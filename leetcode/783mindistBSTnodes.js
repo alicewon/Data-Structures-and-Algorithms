62b2a77d5343874b80a92189c1f873c506adfadb
Alice Won <alicewon4@gmail.com>
2020-07-17 17:14:36 -0700

    leetcode 783

---
+++ leetcode/783mindistBSTnodes.js
@@ -0,0 +1,54 @@
+// to get to 15: 
+// root.right.left.val
+
+// breadth first search
+
+//         1               1 
+//      /   \            /  \
+//     2     3          2    3
+//      \   / \        / \  / \
+//       4  5 6        4 5  6 7
+//                    /\ /\ /\ /\
+      
+
+// [1] -> [X]+[2,3] -> [2,3]
+// [2] -> [X,3]+[4,5] -> [3,4,5]
+// [3] -> [X,4,5]+[6,7] -> [4,5,6,7]
+// [4] -> [X,5,6,7]+[] -> [5,6,7]
+// [5] -> [X,6,7]+[] -> [6,7]
+
+// boilerplate for BFS:
+
+// let q = [root]
+// while (q.length > 0) {
+//     node = q.shift()
+//     if (node.left) q.push(node.left)
+//     if (node.right) q.push(node.right)
+// }
+
+
+// boilerplate for BFS with rows:
+
+// let q = [root], length
+// while (q.length > 0) {
+//     length = q.length
+//     for (let i = 0; i < length; i++) {
+//         node = q.shift()
+//         if (node.left) q.push(node.left)
+//         if (node.right) q.push(node.right)
+//     }
+// } 
+
+let q = [root], length, row, ans = []
+while (q.length > 0) {
+    length = q.length
+    row = 0
+    for (let i = 0; i < length; i++) {
+        node = q.shift()
+        row += node.val
+        if (node.left) q.push(node.left)
+        if (node.right) q.push(node.right)
+    }
+    ans.push(row / length)
+}
+return ans
